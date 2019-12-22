0299fecbf6880796dcfc9c424e310968a8199885
Ranjith Ramalingam <ranjith@Ranjiths-MacBook-Pro.local>
2019-12-21 23:36:29 -0800

    Insert Element at the last in the linked list

--- src/main/java/mylearning/datastructures/LinkedList/LinkedList.java
+++ src/main/java/mylearning/datastructures/LinkedList/LinkedList.java
@@ -5,20 +5,31 @@ public class LinkedList {
 
     public static void main(String arg[]) {
         LinkedList list = new LinkedList();
+        System.out.println(list.head != null ? list.head.data : "Head is Empty on List");
         for (int i = 0; i < 5; i++) {
             list.insertAtFirst(i);
+
+
         }
         list.print();
+        LinkedList list1 = new LinkedList();
+
+        System.out.println(list1.head != null ? list1.head.data : "Head is Empty on List1");
+
+        for (int i = 0; i < 5; i++) {
+            list1.addAtLast(i);
+
+        }
+
+        list1.print();
     }
 
     private void print() {
         Node temp = head;
-        System.out.println(head.data + " fdfdfd " + head.link);
-        while (temp.link != null) {
+        while (temp != null) {
             System.out.println(temp.data);
             temp = temp.link;
         }
-
     }
 
     private void insertAtFirst(int data) {
@@ -33,7 +44,29 @@ public class LinkedList {
             head = newNode;
         }
 
-        System.out.println("Item Inserted : " + newNode.data);
+        System.out.println("Item inserted at First : " + newNode.data);
+    }
+
+    private void addAtLast(int data) {
+        Node newNode = new Node();
+        newNode.data = data;
+        newNode.link = null;
+
+
+        if (head == null) {
+            head = newNode;
+            System.out.println("Item inserted at Last : " + newNode.data);
+            return;
+        }
+        Node temp = head;
+
+        while (temp.link != null) {
+            temp = temp.link;
+        }
+
+        temp.link = newNode;
+
+        System.out.println("Item inserted at Last : " + newNode.data);
     }
 
 
