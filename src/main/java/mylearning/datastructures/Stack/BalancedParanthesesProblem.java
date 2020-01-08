e6b5e229ec5e539d2729f9f4e342a2bb3260bffe
Ranjith Ramalingam <ranjith@Ranjiths-MacBook-Pro.local>
2020-01-07 21:42:54 -0800

    Finding mismathcing parantheses

--- src/main/java/mylearning/datastructures/Stack/BalancedParanthesesProblem.java
+++ src/main/java/mylearning/datastructures/Stack/BalancedParanthesesProblem.java
@@ -1,4 +1,37 @@
 package mylearning.datastructures.Stack;
 
+
+import java.util.Stack;
+
 public class BalancedParanthesesProblem {
+
+    public static void main(String[] str){
+        BalancedParanthesesProblem obj = new BalancedParanthesesProblem();
+        System.out.println(obj.isBalancedParantheses("[(a+B)()]}"));
+
+    }
+
+    private boolean isPair(char open, char close){
+      return  (open == '{' && close == '}'
+        || open == '[' && close == ']'
+        || open == '(' && close == ')') ? true : false;
+
+    }
+
+    private boolean isBalancedParantheses(String s) {
+        Stack<Character> stack = new Stack();
+        for(int i=0 ; i<s.length(); i++){
+            if(s.charAt(i) == '{' ||  s.charAt(i) == '[' || s.charAt(i) == '('){
+                stack.push(s.charAt(i));
+            }else if(s.charAt(i) == '}' ||  s.charAt(i) == ']' || s.charAt(i) == ')'){
+                if(stack.empty() || !isPair(stack.peek() , s.charAt(i))){
+                    return false;
+                }else{
+                    stack.pop();
+                }
+            }
+
+        }
+        return stack.empty() ? true : false;
+    }
 }
--- src/main/java/mylearning/datastructures/Stack/ReverseStringProblemUsingStack.java
+++ src/main/java/mylearning/datastructures/Stack/ReverseStringProblemUsingStack.java
@@ -1,4 +1,59 @@
 package mylearning.datastructures.Stack;
+import java.util.Stack;
 
 public class ReverseStringProblemUsingStack {
+
+
+    public String reverse(String input, int length){
+        Stack<Character> stack = new Stack();
+
+        for(int i = 0 ; i< input.length() ; i++){
+            stack.push(input.charAt(i));
+        }
+
+        StringBuffer stringBuffer = new StringBuffer();
+        for(int i=0 ;i<length; i ++){
+            stringBuffer.append(stack.peek());
+            stack.pop();
+        }
+        return stringBuffer.toString();
+
+    }
+
+    public String reverseUsingTwoPositionWay(String input){
+        char[] inputarray = input.toCharArray();
+        int min_index = 0;
+        int max_index = input.length()-1;
+        while(min_index < max_index){
+            char temp = inputarray[min_index];
+            inputarray[min_index] = inputarray[max_index];
+            inputarray[max_index] = temp;
+            min_index ++ ;
+            max_index -- ;
+        }
+
+        return (new String(inputarray));
+    }
+
+
+    public static void main(String[] arg){
+
+        ReverseStringProblemUsingStack reverse = new ReverseStringProblemUsingStack();
+
+        String input = "MOM DAD IS IN THE OFFICE";
+
+
+        System.out.println("|-------------------------------------------------|");
+        System.out.println("|       " + reverse.reverse(input, input.length()));
+        System.out.println("|-------------------------------------------------|");
+
+        System.out.println("Reverse Using.. efficient way");
+        System.out.println();
+        System.out.println("|-------------------------------------------------|");
+        System.out.println(reverse.reverseUsingTwoPositionWay("Kanna Lattu Thinna aasaya"));
+        System.out.println("|-------------------------------------------------|");
+
+
+
+    }
 }
